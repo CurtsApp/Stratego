303a013a6bb2ab496a05dfbd4aba8ccd20b59345
cknuth1 <33706019+cknuth1@users.noreply.github.com>
2018-04-29 06:11:21 -0700

    Add files via upload
    exception update

--- Stratego/src/edu/asu/stratego/game/ClientSocket.java
+++ Stratego/src/edu/asu/stratego/game/ClientSocket.java
@@ -1,14 +1,23 @@
 package edu.asu.stratego.game;
 
-import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+
+import edu.asu.stratego.gui.ConnectionScene;
+
 
 /**
  * Client socket that connects to the server. The client should only use one 
  * socket to connect to the server.
  */
 public final class ClientSocket {
+	
+    private static BufferedReader stdin;
+	private static FileWriter writer;
+	private static ArrayList<String> list;
     
     private static Socket socket = null;
     
@@ -18,15 +27,67 @@ public final class ClientSocket {
     private ClientSocket() { /* Intentionally Empty */ }
     
     /**
-     * Attempts a connection to the server.
+     * Attempts a connection to the server and updates saved server ips file
      * 
      * @param serverIP server IP address
      * @param port server port number
      */
     public static void connect(String serverIP, int port) 
             throws UnknownHostException, IOException {
-        socket = new Socket(serverIP, port);
-    }
+        socket = new Socket(serverIP, port); //attempt connection
+    	
+        //take inputs from saved server file
+		stdin = new BufferedReader(new InputStreamReader(new FileInputStream(ConnectionScene.file)));
+		writer = new FileWriter(ConnectionScene.file,true);		
+		list = new ArrayList<String>();		
+		String input = stdin.readLine();		
+		while(input != null)
+		{
+			list.add(input);		
+			input = stdin.readLine();
+		}
+		stdin.close();
+		
+		//if ip was valid and not already present in save file, write ip to save file
+        if(ConnectionScene.isValid(serverIP) && !list.contains(encrypt(serverIP))) 
+        {
+        	try 
+        	{
+        		writer.write(encrypt(serverIP) + System.getProperty("line.separator"));
+        		writer.flush();
+        		writer.close();
+			} 
+        	catch (IOException e1) 
+        	{
+				e1.printStackTrace();
+			}
+        }  
+    }    
+    
+    /**
+    * encrypts server ip to prevent easy tampering
+    *
+    * @return encrypted value of a server ip
+    */
+	public static String encrypt(String entered)
+	{
+		String result = "";
+
+		char[] values = entered.toCharArray();
+
+		int[] encrypted = new int[values.length];
+
+		for(int i = 0; i < values.length; i++)
+		{
+			encrypted[i] = (int)Math.pow((int)values[i],2);
+
+			result += Integer.toString(encrypted[i]) + "$";
+		}
+
+		return ConnectionScene.reverse(result);
+	}
+
+    
     
     /**
      * Returns the one and only instance of the client socket.
